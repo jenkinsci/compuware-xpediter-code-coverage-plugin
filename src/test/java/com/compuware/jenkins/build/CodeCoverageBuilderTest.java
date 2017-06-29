77fe03a40f7e15cf1d7168507e788eb3335d6fa5
PFHJYG0 <jon.goering@compuware.com>
2017-06-29 08:00:04 -0400

    Updated tests

--- src/test/java/com/compuware/jenkins/build/CodeCoverageBuilderTest.java
+++ src/test/java/com/compuware/jenkins/build/CodeCoverageBuilderTest.java
@@ -17,14 +17,11 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import com.compuware.jenkins.build.CodeCoverageBuilder.CodeCoverageDescriptorImpl;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.FreeStyleBuild;
@@ -38,18 +35,22 @@ import hudson.model.TaskListener;
 @SuppressWarnings("nls")
 public class CodeCoverageBuilderTest
 {
-
 	@Rule
 	public JenkinsRule j = new JenkinsRule();
-	
+
+	// TODO (pfhjyg0) : to be handled later, when actually performing Code Coverage.
 	private TestScanner m_testScanner = null;
-	
+
 	@Before
 	public void setup()
 	{
+		// TODO (pfhjyg0) : to be handled later, when actually performing Code Coverage.
 		m_testScanner = new TestScanner(null);
 	}
 
+	/**
+	 * Tests the construction of a builder, verifying configuration values.
+	 */
 	@Test
 	public void constructBuilderTest()
 	{
@@ -57,100 +58,129 @@ public class CodeCoverageBuilderTest
 		String expectedCredentialsId = "pfhvvv0";
 		String expectedAnalysisPropertiesPath = "/a/path/to/analysis/properties/ccanalysis.properties";
 		String expectedAnalysisProperties = "cc.source=/src\ncc.repos=pfhjyg0.xv20.reposit\ncc.system=\ncc.test=\ncc.ddio.override=";
-		
-		CodeCoverageBuilder builder = new CodeCoverageBuilder(expectedHostConnection, expectedCredentialsId, expectedAnalysisPropertiesPath, expectedAnalysisProperties);
-		
+
+		CodeCoverageBuilder builder = new CodeCoverageBuilder(expectedHostConnection, expectedCredentialsId,
+				expectedAnalysisPropertiesPath, expectedAnalysisProperties);
+
 		assertThat(String.format("Expected CodeCoverageBuilder.getHostConnection() to return %s", expectedHostConnection),
 				builder.getHostConnection(), is(equalTo(expectedHostConnection)));
-		
+
 		assertThat(String.format("Expected CodeCoverageBuilder.getCredentialsId() to return %s", expectedCredentialsId),
 				builder.getCredentialsId(), is(equalTo(expectedCredentialsId)));
-		
-		assertThat(String.format("Expected CodeCoverageBuilder.getAnalysisPropertiesPath() to return %s", expectedAnalysisPropertiesPath),
+
+		assertThat(
+				String.format("Expected CodeCoverageBuilder.getAnalysisPropertiesPath() to return %s",
+						expectedAnalysisPropertiesPath),
 				builder.getAnalysisPropertiesPath(), is(equalTo(expectedAnalysisPropertiesPath)));
-		
-		assertThat(String.format("Expected CodeCoverageBuilder.getAnalysisProperties() to return %s", expectedAnalysisProperties),
+
+		assertThat(
+				String.format("Expected CodeCoverageBuilder.getAnalysisProperties() to return %s", expectedAnalysisProperties),
 				builder.getAnalysisProperties(), is(equalTo(expectedAnalysisProperties)));
 	}
 
-//	@Test
-//	public void descriptorValuesTest()
-//	{
-//		CodeCoverageBuilder builder = new CodeCoverageBuilder(null, null, null);
-//		CodeCoverageDescriptorImpl descriptor = (CodeCoverageDescriptorImpl) builder.getDescriptor();
-//		
-//		String displayName = descriptor.getDisplayName();
-//		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDisplayName() to not be null.", displayName, is(notNullValue()));
-//		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDisplayName() to not be empty.", displayName.isEmpty(), is(false));
-//		
-//		String defaultAnalysisPropertiesPath = descriptor.getDefaultAnalysisPropertiesPath();
-//		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisPropertiesPath() to not be null.", defaultAnalysisPropertiesPath, is(notNullValue()));
-//		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisPropertiesPath() to not be empty.", defaultAnalysisPropertiesPath.isEmpty(), is(false));
-//		
-//		String defaultAnalysisProperties = descriptor.getDefaultAnalysisProperties();
-//		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisProperties() to not be null.", defaultAnalysisProperties, is(notNullValue()));
-//		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisProperties() to not be empty.", defaultAnalysisProperties.isEmpty(), is(false));
-//	}
-	
-//	@Test
-//	public void executionTest()
-//	{
-//		String expectedHostConnection = "TestConnection";
-//		String expectedAnalysisPropertiesPath = "/a/path/to/analysis/properties/ccanalysis.properties";
-//		String expectedAnalysisProperties = "cc.source=/src\ncc.repos=pfhjyg0.xv20.reposit\ncc.system=\ncc.test=\ncc.ddio.override=";
-//
-//		String logFileOutput = null;
-//		try
-//		{
-//			FreeStyleProject project = j.createFreeStyleProject("TestProject");
-//			project.getBuildersList().add(new CodeCoverageBuilder(expectedHostConnection, expectedAnalysisPropertiesPath, expectedAnalysisProperties));
-//			FreeStyleBuild build = project.scheduleBuild2(0).get();
-//			logFileOutput = FileUtils.readFileToString(build.getLogFile());
-//
-//			assertThat(String.format("Expected log to contain %s.", expectedHostConnection), logFileOutput,
-//					containsString(expectedHostConnection));
-//
-//			assertThat(String.format("Expected log to contain %s.", expectedAnalysisPropertiesPath), logFileOutput,
-//					containsString(expectedAnalysisPropertiesPath));
-//
-//			assertThat(String.format("Expected log to contain %s.", expectedAnalysisProperties), logFileOutput,
-//					containsString(expectedAnalysisProperties));		
-//		}
-//		catch (IOException | InterruptedException | ExecutionException e)
-//		{
-//			fail(e.getMessage());
-//		}
-//	}
+	/**
+	 * Tests build descriptor values, such as default values and display name.
+	 */
+	@Test
+	public void descriptorValuesTest()
+	{
+		CodeCoverageBuilder builder = new CodeCoverageBuilder(null, null, null, null);
+		CodeCoverageDescriptorImpl descriptor = (CodeCoverageDescriptorImpl) builder.getDescriptor();
 
-//	@Test
-//	public void roundTripTest()
-//	{
-//		String expectedHostConnection = "cw09.compuware.com";
-//		String expectedCredentialsId = "pfhvvv0";
-//		String expectedAnalysisPropertiesPath = "/a/path/to/analysis/properties/ccanalysis.properties";
-//		String expectedAnalysisProperties = "cc.source=/src\ncc.repos=pfhjyg0.xv20.reposit\ncc.system=\ncc.test=\ncc.ddio.override=";
-//
-//		try
-//		{
-//			FreeStyleProject project = j.createFreeStyleProject("TestProject");
-//			CodeCoverageBuilder before = new CodeCoverageBuilder(expectedHostConnection, expectedCredentialsId, expectedAnalysisPropertiesPath, expectedAnalysisProperties);
-//			project.getBuildersList().add(before);
-//			
-//			j.configRoundtrip(project);
-//			j.assertLogContains("hostConnection,credentialsId,analysisPropertiesPath,analysisProperties,...", j.buildAndAssertSuccess(project));
-//			
-////			HtmlPage page = j.submit(j.createWebClient().getPage(project,"configure").getFormByName("config"));
-////
-////			CodeCoverageBuilder after = project.getBuildersList().get(CodeCoverageBuilder.class);
-////
-////			j.assertEqualBeans(before,after,"hostConnection,analysisPropertiesPath,analysisProperties,...");			
-//		}
-//		catch (Exception e)
-//		{
-//			fail(e.getMessage());
-//		}
-//	}
+		String displayName = descriptor.getDisplayName();
+		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDisplayName() to not be null.", displayName,
+				is(notNullValue()));
+		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDisplayName() to not be empty.", displayName.isEmpty(),
+				is(false));
 
+		String defaultAnalysisPropertiesPath = descriptor.getDefaultAnalysisPropertiesPath();
+		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisPropertiesPath() to not be null.",
+				defaultAnalysisPropertiesPath, is(notNullValue()));
+		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisPropertiesPath() to not be empty.",
+				defaultAnalysisPropertiesPath.isEmpty(), is(false));
+
+		String defaultAnalysisProperties = descriptor.getDefaultAnalysisProperties();
+		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisProperties() to not be null.",
+				defaultAnalysisProperties, is(notNullValue()));
+		assertThat("Expected CodeCoverageBuilder.DescriptorImpl.getDefaultAnalysisProperties() to not be empty.",
+				defaultAnalysisProperties.isEmpty(), is(false));
+	}
+
+	/**
+	 * Tests the results of an execution.
+	 * 
+	 * <p>
+	 * A project is created, configured and executed where the log is examined to verify results.
+	 */
+	@Test
+	public void executionTest()
+	{
+		String expectedHostConnection = "TestConnection";
+		String expectedCredentialsId = "pfhvvv0";
+		String expectedAnalysisPropertiesPath = "/a/path/to/analysis/properties/ccanalysis.properties";
+		String expectedAnalysisProperties = "cc.source=/src\ncc.repos=pfhjyg0.xv20.reposit\ncc.system=\ncc.test=\ncc.ddio.override=";
+
+		try
+		{
+			FreeStyleProject project = j.createFreeStyleProject("TestProject");
+			project.getBuildersList().add(new CodeCoverageBuilder(expectedHostConnection, expectedCredentialsId,
+					expectedAnalysisPropertiesPath, expectedAnalysisProperties));
+
+			FreeStyleBuild build = j.buildAndAssertSuccess(project);
+
+			// Could use JenkinsRule.java#assertLogContains(String, Run), but message on failure was odd regarding expected value.
+
+			String logFileOutput = JenkinsRule.getLog(build);
+
+			assertThat(String.format("Expected log to contain Host connection: \"%s\".", expectedHostConnection), logFileOutput,
+					containsString(expectedHostConnection));
+
+			assertThat(String.format("Expected log to contain Login credentials: \"%s\".", expectedCredentialsId),
+					logFileOutput, containsString(expectedCredentialsId));
+
+			assertThat(
+					String.format("Expected log to contain Analysis properties path: \"%s\".", expectedAnalysisPropertiesPath),
+					logFileOutput, containsString(expectedAnalysisPropertiesPath));
+
+			assertThat(String.format("Expected log to contain Analysis properties: \"%s\".", expectedAnalysisProperties),
+					logFileOutput, containsString(expectedAnalysisProperties));
+		}
+		catch (Exception e)
+		{
+			fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * Perform a round trip test on the Code Coverage configuration builder.
+	 * 
+	 * <p>
+	 * A project is created, configured, submitted / saved, and reloaded where the original configuration is compared against
+	 * the reloaded configuration for equality.
+	 */
+	@Test
+	public void roundTripTest()
+	{
+		String expectedHostConnection = "cw09.compuware.com";
+		String expectedCredentialsId = "pfhvvv0";
+		String expectedAnalysisPropertiesPath = "/a/path/to/analysis/properties/ccanalysis.properties";
+		String expectedAnalysisProperties = "cc.source=/src\ncc.repos=pfhjyg0.xv20.reposit\ncc.system=\ncc.test=\ncc.ddio.override=";
+
+		try
+		{
+			FreeStyleProject project = j.createFreeStyleProject("TestProject");
+			CodeCoverageBuilder before = new CodeCoverageBuilder(expectedHostConnection, expectedCredentialsId,
+					expectedAnalysisPropertiesPath, expectedAnalysisProperties);
+			project.getBuildersList().add(before);
+			j.assertEqualBeans(before, j.configRoundtrip(before), "hostConnection,analysisPropertiesPath,analysisProperties");
+		}
+		catch (Exception e)
+		{
+			fail(e.getMessage());
+		}
+	}
+
+	// TODO (pfhjyg0) : to be handled later, when actually performing Code Coverage.
 	private class TestScanner extends CodeCoverageScanner
 	{
 		/**
