bab5e56753421e0117fb9f464dc72f78c414b047
Dave Dresser <ddresser@bmc.com>
2022-03-23 12:20:30 -0400

    ZENG-253105 Update copyright

--- src/main/java/com/compuware/jenkins/build/RemoteSystemProperties.java
+++ src/main/java/com/compuware/jenkins/build/RemoteSystemProperties.java
@@ -1,7 +1,8 @@
 /**
  * The MIT License (MIT)
  * 
- * Copyright (c) 2016-2022 Compuware Corporation
+ * Copyright (c) 2016, 2017 Compuware Corporation
+ * (c) Copyright 2016-2017, 2022 BMC Software, Inc.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
  * and associated documentation files (the "Software"), to deal in the Software without restriction, 
